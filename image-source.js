9a6fc505d999ef51183dabc1b624e497ecec458e
Octavio Turra <octavio@guiabolso.com.br>
2016-07-06 16:22:19 -0300

    Basic image source file

---
+++ image-source.js
@@ -0,0 +1,58 @@
+function getImages() {
+  return {
+    animals: [{
+     url: 'http://lorempixel.com/400/400/animals?1',
+     title: 'Imagem 1'
+    },{
+     url: 'http://lorempixel.com/400/400/animals?2',
+     title: 'Imagem 2'
+    },{
+     url: 'http://lorempixel.com/400/400/animals?3',
+     title: 'Imagem 3'
+    },{
+     url: 'http://lorempixel.com/400/400/animals?4',
+     title: 'Imagem 4'
+    }],
+    city: [{
+     url: 'http://lorempixel.com/400/400/city?1',
+     title: 'Imagem 1'
+    },{
+     url: 'http://lorempixel.com/400/400/city?2',
+     title: 'Imagem 2'
+    },{
+     url: 'http://lorempixel.com/400/400/city?3',
+     title: 'Imagem 3'
+    }],
+    people: [{
+     url: 'http://lorempixel.com/400/400/people?1',
+     title: 'Imagem 1'
+    },{
+     url: 'http://lorempixel.com/400/400/people?2',
+     title: 'Imagem 2'
+    },{
+     url: 'http://lorempixel.com/400/400/people?3',
+     title: 'Imagem 3'
+    },{
+     url: 'http://lorempixel.com/400/400/people?4',
+     title: 'Imagem 4'
+    },{
+     url: 'http://lorempixel.com/400/400/people?5',
+     title: 'Imagem 5'
+    },{
+     url: 'http://lorempixel.com/400/400/people?6',
+     title: 'Imagem 6'
+    }],
+    transport: [{
+     url: 'http://lorempixel.com/400/400/transport?1',
+     title: 'Imagem 1'
+    },{
+     url: 'http://lorempixel.com/400/400/transport?2',
+     title: 'Imagem 2'
+    }] 
+  };
+}
+
+function getImagesByCategory(category) { 
+  var images = getImages();
+  return images[category] || []; 
+}
